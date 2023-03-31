2818c1b6870a9f09c4feec41945dbbc3cb8a5b56
M. Kabir Hassan <61631611+n3o-d4rk3r@users.noreply.github.com>
2023-04-01 00:24:47 +0600

    Add files via upload

---
+++ plist_data.sql
@@ -0,0 +1,5 @@
+SELECT 
+  ExtractValue(plist_field, '//key_name_1') AS extracted_field_1,
+  ExtractValue(plist_field, '//key_name_2') AS extracted_field_2,
+  ExtractValue(plist_field, '//key_name_3') AS extracted_field_3
+FROM table_name;
